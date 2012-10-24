87e1688d99203d48252ef7cf207a698b0b9d99d8
Tamerlan <tamerjlah@gmail.com>
2012-10-24 03:09:58 +0300

    frg dynamic

--- gen/tryby/and/tresh/R.java
+++ gen/tryby/and/tresh/R.java
@@ -16,7 +16,7 @@ public final class R {
     }
     public static final class id {
         public static final int edit_message=0x7f070000;
-        public static final int headlines_fragment=0x7f070001;
+        public static final int frg_cantainer=0x7f070001;
         public static final int menu_settings=0x7f070002;
     }
     public static final class layout {
--- src/tryby/and/tresh/MainActivity.java
+++ src/tryby/and/tresh/MainActivity.java
@@ -9,17 +9,41 @@ import android.widget.EditText;
 
 public class MainActivity extends FragmentActivity {
 	public final static String EXTRA_MESSAGE = "tryby.and.tresh.MESSAGE";
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-	
-	@Override
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_fragment);
+        setContentView(R.layout.activity_main);
+        
+        
+     // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.frg_cantainer) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create an instance of ExampleFragment
+            MainFragment firstFragment = new MainFragment();
+            
+            // In case this activity was started with special instructions from an Intent,
+            // pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+            
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frg_cantainer, firstFragment).commit();
+        }
     }
+	
+//	@Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main_fragment);
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
