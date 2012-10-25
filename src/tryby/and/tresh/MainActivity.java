8dc79c455114ce5df66fa6adb46f115efcfed9a6
Tamerlan <tamerjlah@gmail.com>
2012-10-26 02:42:51 +0300

    Better understanding now

--- src/tryby/and/tresh/MainActivity.java
+++ src/tryby/and/tresh/MainActivity.java
@@ -1,61 +1,57 @@
 package tryby.and.tresh;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
+import android.text.InputType;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
-public class MainActivity extends FragmentActivity {
-	public final static String EXTRA_MESSAGE = "tryby.and.tresh.MESSAGE";
+
+public class MainActivity extends Activity {
+
+	Button btn1;
+	EditText tf1;
+	int counter = 0;
+	
+	CheckBox cb_1;
+	CheckBox cb_2;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.a_main);
+      
+        cb_1 = (CheckBox)findViewById(R.id.cb_color);
+        cb_2 = (CheckBox)findViewById(R.id.cb_bold);
+
         
+        btn1 = (Button)findViewById(R.id.btn_1);
+        tf1 = (EditText)findViewById(R.id.et_1);
         
-     // Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
-        if (findViewById(R.id.frg_cantainer) != null) {
-
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
-                return;
-            }
-
-            // Create an instance of ExampleFragment
-            MainFragment firstFragment = new MainFragment();
-            
-            // In case this activity was started with special instructions from an Intent,
-            // pass the Intent's extras to the fragment as arguments
-            firstFragment.setArguments(getIntent().getExtras());
-            
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.frg_cantainer, firstFragment).commit();
-        }
-    }
-	
-//	@Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main_fragment);
-//    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    public void sendMessage(View view) {
-      Intent intent = new Intent(this, DisplayMessageActivity.class);
-      EditText editText = (EditText) findViewById(R.id.edit_message);
-      String message = editText.getText().toString();
-      intent.putExtra(EXTRA_MESSAGE, message);
-      startActivity(intent);
-    }
+        tf1.setSingleLine();
+        tf1.setInputType(InputType.TYPE_NULL);
+        
+        btn1.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				
+				if(cb_1.isChecked())
+					tf1.setTextColor(Color.RED);
+				else
+					tf1.setTextColor(Color.BLACK);
+				
+				if(cb_2.isChecked())
+					tf1.setTypeface(Typeface.DEFAULT_BOLD);
+				else
+					tf1.setTypeface(Typeface.DEFAULT);
+				
+				tf1.setText("Clicked"+ ++counter);
+			}
+		}); 
+     }
 }
