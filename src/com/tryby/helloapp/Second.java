fae90cd9bbf8d70fb3f47886e9d2e8b3ff8f8d71
Tamerlan-i7 <trybynenko@gmail.com>
2013-12-12 01:03:15 +0200

    second activity

---
+++ src/com/tryby/helloapp/Second.java
@@ -0,0 +1,14 @@
+package com.tryby.helloapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+	}
+
+}
