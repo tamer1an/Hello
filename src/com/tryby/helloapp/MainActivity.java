3695ffb242bb7cd0c4fac610efaee84a9a4c3958
Tamerlan-i7 <trybynenko@gmail.com>
2013-12-12 16:34:30 +0200

    -implicit/explicit intents -service for WIFI with permmisions

--- src/com/tryby/helloapp/MainActivity.java
+++ src/com/tryby/helloapp/MainActivity.java
@@ -1,27 +1,62 @@
 package com.tryby.helloapp;
 
+import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	//create event handler
         super.onCreate(savedInstanceState);
+        
+        //assign to layout.xml
         setContentView(R.layout.activity_main);
+        
+        // find button and attach onClick handler
+        final EditText et = (EditText) findViewById(R.id.editText1);
         
         Button b = (Button) findViewById(R.id.lunchSecond);
         b.setOnClickListener(new OnClickListener() {			
 			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this, Second.class));				
+				Intent intent = new Intent(MainActivity.this, Second.class);
+				intent.putExtra("text", et.getText().toString());				
+					
+				 startActivity(new Intent(intent));				
 			}
-		});
+		});  
+        
+        //change pic to other than default pic in xml
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView.setImageResource(R.drawable.test);
+        
+       
+        // check wi-fi connectivity
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        TextView tw = (TextView) findViewById(R.id.textView1);
+        boolean wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnectedOrConnecting();
+        if(wifi){
+        	tw.setText("wifi is on");
+        }else{
+        	tw.setText("wifi is off");
+        }
+        
+        
+// Implicit Intent
+//        ImageView iv = (ImageView) findViewById(R.id.imageView1);
+//        iv.setImageURI((Uri) getIntent().getExtras().get(Intent.EXTRA_STREAM));
+        
     }
 
     @Override
--- src/com/tryby/helloapp/Second.java
+++ src/com/tryby/helloapp/Second.java
@@ -2,6 +2,7 @@ package com.tryby.helloapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Second extends Activity {
 
@@ -9,6 +10,9 @@ public class Second extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.second);
+		
+		TextView tv = (TextView) findViewById(R.id.textView2);
+		tv.setText(getIntent().getExtras().getString("text"));
+		
 	}
-
 }
