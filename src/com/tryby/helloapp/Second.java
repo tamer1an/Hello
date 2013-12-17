aee2f20d4ec6e0d70febef6229550fde43b554db
Tamerlan-i7 <trybynenko@gmail.com>
2013-12-17 12:42:08 +0200

    custom dialog

--- src/com/tryby/helloapp/Second.java
+++ src/com/tryby/helloapp/Second.java
@@ -1,7 +1,12 @@
 package com.tryby.helloapp;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class Second extends Activity {
@@ -13,6 +18,17 @@ public class Second extends Activity {
 		
 		TextView tv = (TextView) findViewById(R.id.textView2);
 		tv.setText(getIntent().getExtras().getString("text"));	
+		
+		tv.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Dialog d = new Dialog(Second.this);
+				d.setContentView(R.layout.dialog);
+				d.setTitle("This is important");
+				d.show();
+			}
+		});
 		
 	}
 }
