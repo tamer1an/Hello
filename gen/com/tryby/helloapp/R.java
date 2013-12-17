2e8057568d92e208ef69567d324652baf01464a9
Tamerlan-i7 <trybynenko@gmail.com>
2013-12-18 01:47:05 +0200

    - Notification in top bar - Toast message

--- gen/com/tryby/helloapp/R.java
+++ gen/com/tryby/helloapp/R.java
@@ -30,25 +30,26 @@ public final class R {
         public static final int usa=0x7f020009;
     }
     public static final class id {
-        public static final int button1=0x7f090006;
-        public static final int button2=0x7f09000c;
-        public static final int checkBox1=0x7f09000d;
+        public static final int button1=0x7f09000b;
+        public static final int button111=0x7f090006;
+        public static final int button2=0x7f09000d;
+        public static final int checkBox1=0x7f09000e;
         public static final int editText1=0x7f090002;
-        public static final int imageView0=0x7f09000f;
+        public static final int imageView0=0x7f090010;
         public static final int imageView1=0x7f090003;
         public static final int imageView2=0x7f090004;
         public static final int imageView7=0x7f090007;
-        public static final int item1=0x7f090011;
-        public static final int item2=0x7f090012;
+        public static final int item1=0x7f090012;
+        public static final int item2=0x7f090013;
         public static final int lunchSecond=0x7f090001;
-        public static final int menu_settings=0x7f090010;
+        public static final int menu_settings=0x7f090011;
         public static final int seekBar1=0x7f090005;
         public static final int tableRow1=0x7f09000a;
-        public static final int tableRow2=0x7f09000e;
+        public static final int tableRow2=0x7f09000f;
         public static final int textView1=0x7f090000;
         public static final int textView2=0x7f090009;
         public static final int textView7=0x7f090008;
-        public static final int toggleButton1=0x7f09000b;
+        public static final int toggleButton1=0x7f09000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/tryby/helloapp/Second.java
+++ src/com/tryby/helloapp/Second.java
@@ -2,12 +2,20 @@ package com.tryby.helloapp;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Second extends Activity {
 
@@ -27,8 +35,37 @@ public class Second extends Activity {
 				d.setContentView(R.layout.dialog);
 				d.setTitle("This is important");
 				d.show();
+				
+				
+			}
+		});	
+		
+		TextView tv2 = (TextView) findViewById(R.id.textView1);
+		tv2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+					NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+					Notification notify = new Notification( android.R.drawable.stat_notify_more, "This is test msg", System.currentTimeMillis());
+					
+					Context context = Second.this;
+					CharSequence title = "You have been notified";
+					CharSequence details = "continue..";
+					
+					Intent intent = new Intent();
+					PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+					
+					notify.setLatestEventInfo(context, title, details, pendingIntent);
+//					notify.sound = Uri.parse("android.resource://com.tryby.helloapp/"+ R.raw)
+					
+					nm.notify(0, notify);
+					
+					Toast toast = Toast.makeText(Second.this, "notification create", 5000);
+					toast.setGravity(Gravity.CENTER, 0, 0);
+					toast.show();
+				
 			}
 		});
-		
 	}
 }
