0ce504732a830c0cbb746600fd515889b24ae324
tamer1an <trybynenko@gmail.com>
2014-04-13 17:33:05 +0300

    new activity
    new activity and renamed id's  for loginActivity

--- visonic2go/interactive/src/main/java/com/tyco/visonic/interactive/LoginActivity.java
+++ visonic2go/interactive/src/main/java/com/tyco/visonic/interactive/LoginActivity.java
@@ -1,15 +1,30 @@
 package com.tyco.visonic.interactive;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.login){
+            Log.v("LEE", "Button1");
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(new Intent(intent));
+        }
     }
 }
