c7683db37493b9e45fd73dfd5fa5ca0010dd03fe
Trybynenko Andrii <trybynenko@gmail.com>
2014-04-14 22:58:47 +0300

    finish loginActivity event
    finish loginActivity event

--- visonic2go/interactive/src/main/java/com/tyco/visonic/interactive/LoginActivity.java
+++ visonic2go/interactive/src/main/java/com/tyco/visonic/interactive/LoginActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+
+//import android.view.Menu;
+//import android.view.MenuItem;
 
 public class LoginActivity extends Activity implements View.OnClickListener {
 
@@ -15,12 +17,12 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        Button btn = (Button) findViewById(R.id.login);
+        btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
         if(v.getId() == R.id.login){
             Log.v("LEE", "Button1");
             Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
